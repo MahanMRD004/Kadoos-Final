2918084e72b7dbf852fb5fb852c9084ebe11d471
MahanMRD004 <90115714+MahanMRD004@users.noreply.github.com>
2021-12-02 00:16:04 +0330

    Add files via upload

--- js/laptop.js
+++ js/laptop.js
@@ -3,8 +3,7 @@ const changeImage2 = document.getElementById("changeImg2");
 const changeImage3 = document.getElementById("changeImg3");
 const changeImage4 = document.getElementById("changeImg4");
 const changeImage5 = document.getElementById("changeImg5");
-const thumbnail = document.getElementById("thumb")
-
+const thumbnail = document.getElementById("thumb");
 
 changeImage1.addEventListener("click", function(){
     $(thumbnail).attr("src","files/products/laptop/1-1.jpg")
@@ -20,4 +19,21 @@ changeImage4.addEventListener("click", function(){
 });
 changeImage5.addEventListener("click", function(){
     $(thumbnail).attr("src","files/products/laptop/1-5.jpg")
+});
+
+$("#toggleAdd").click(function(){
+    $(".productInfo").toggleClass("active");
+    $(".addToCart").toggleClass("active");
+    $(".selectColor").toggleClass("active");
+    $("#add").toggleClass("active")
+    if (this.innerHTML === "انتخاب رنگ") {
+        this.innerHTML = "انصراف";
+      } else {
+        this.innerHTML = "انتخاب رنگ";
+      }
+});
+
+$(".color").click(function(){
+    $(this).addClass("selected");
+    $(this).siblings().removeClass("selected");
 });
